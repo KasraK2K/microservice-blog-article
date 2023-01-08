60fa2a15ed9ee45ed8cd53a8796bd8959a8fd75d
kasrakaramikk <kasra.karami.kk@gmail.com>
2023-01-08 14:00:32 +0330

    Add swagger
    I added swagger for this project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 import { NestFactory } from '@nestjs/core';
-import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from '@nestjs/platform-fastify';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
 
@@ -10,10 +8,16 @@ async function bootstrap() {
   const logger = new Logger(bootstrap.name);
 
   const port = process.env.PORT || 3001;
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter(),
-  );
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
+
+  const config = new DocumentBuilder()
+    .setTitle('Other')
+    .setDescription('Article service work on posts')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   await app
     .listen(port, '0.0.0.0')
     .then(() => logger.log(`User service is running on port ${port}`))
